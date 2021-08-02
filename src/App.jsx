e8cd71d9049fbeb169c81599f655bab6517555fa
Benjamin Maudslay <benjaminmaudslay@Benjamins-MacBook-Pro-959.local>
2021-08-02 11:52:14 +0100

    set up button logic ready for evaluate functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
-import styled from "styled-components"
-import buttons from "./buttons.json"
+import { useState } from "react";
+import styled from "styled-components";
+
+import buttons from "./buttons.json";
 
 const App = () => {
+  const [screen, setScreen] = useState([]);
+
+  const handleClick = (val) => {
+    if (val === "clear") {
+      setScreen([0])
+    } else if (val === "=") {
+      console.log("eval this string " + screen.join(""))
+    } else {
+      setScreen([...screen, val])
+    }
+  }
+
   return (
     <div>
-      <div className="screen-wrapper"><h1>0</h1></div>
+      <div className="screen-wrapper">
+        <h1>{screen}</h1>
+      </div>
       <ButtonWrapper>
         {buttons.map((item) => (
-          <StyledButton className={item.style} mathValue={item.value} >{item.displayValue}</StyledButton>
+          <StyledButton
+            className={item.style}
+            onClick={() => handleClick(item.value)}
+          >
+            {item.displayValue}
+          </StyledButton>
         ))}
       </ButtonWrapper>
     </div>
-  )
-}
+  );
+};
 
 const ButtonWrapper = styled.div`
   height: 250px;
   width: 200px;
   display: grid;
-  grid-template-areas: 
-    "clear clear clear ." 
-    ". . . ." 
-    ". . . ." 
-    ". . . ." 
+  grid-template-areas:
+    "clear clear clear ."
+    ". . . ."
+    ". . . ."
+    ". . . ."
     "zero zero . .";
 
   .clear {
@@ -32,13 +53,13 @@ const ButtonWrapper = styled.div`
   .zero {
     grid-area: zero;
   }
-`
+`;
 
 const StyledButton = styled.button`
   /* height: 40px;
   width: 40px;
   margin: 10px */
   /* border-radius: 0; */
-`
+`;
 
 export default App;
